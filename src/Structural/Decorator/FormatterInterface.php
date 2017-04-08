14db8094580d63cfac8e0136578380ae946bbcc3
Slawomir Iwanczuk <iwanczuk@users.noreply.github.com>
2017-04-08 14:11:19 +0200

    Update for formatter comment

--- src/Structural/Decorator/FormatterInterface.php
+++ src/Structural/Decorator/FormatterInterface.php
@@ -5,6 +5,7 @@ namespace PhpDesignPatternsCheatsheet\Structural\Decorator;
 interface FormatterInterface
 {
     /**
+     * @param string $text
      * @return string
      */
     public function format(string $text): string;
